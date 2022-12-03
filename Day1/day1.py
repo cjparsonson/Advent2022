c80dbf6993368da5345c156bf940d7f1887b8d6a
Chris Parsonson <cjparsonson@googlemail.com>
2022-12-03 00:06:59 +0000

    Complete day 1 part 1

---
+++ Day1/day1.py
@@ -0,0 +1,19 @@
+# Day 1 Advent of Code
+
+# Declare an empty list
+data_list = []
+# Open the file
+with open('./inputData.txt', 'r') as file:
+    raw_data = file.read()
+    # Split file into blocks, a double newline is the separator
+    data_blocks = raw_data.split('\n\n')
+    # Loop through the blocks splitting into groups of integers
+    for block in data_blocks:
+        split_block = block.split('\n')
+        int_block = [int(item) for item in split_block]
+        data_list.append(int_block)
+
+# Create a list of summed lists and sort
+totals_list = [sum(list_block) for list_block in data_list]
+sorted_totals = sorted(totals_list, reverse=True)
+print(f"The highest value is: {sorted_totals[0]}")
